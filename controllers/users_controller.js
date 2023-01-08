539818f6b5145d14d6b1114e36a78d0bf913f106
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2023-01-07 23:04:14 -0500

    continuing to flesh out MVC architecture; models and controllers are shells; starting with views

---
+++ controllers/users_controller.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const usersRouter = express.Router()
+
+// INDEX
+
+// NEW
+
+// SHOW
+
+// EDIT
+
+// UPDATE
+
+// CREATE 
+
+// DELETE
+
+module.exports = usersRouter
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 // Dependencies
 const express = require('express')
+const methodOverride = require('method-override')
 
 // Configuration
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+
+// middleware
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// user routes
+const usersController = require('./controllers/users_controller.js')
+app.use('/users', usersController)
+
+// 404 page
+app.get('*', (req, res) => {
+    res.send('404 - see server.js')
+})
 
 // listen
 app.listen(PORT, () => {

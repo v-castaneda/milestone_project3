7de597a35cef4727dd7fcc10cc8afccfce9e3527
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2023-01-08 00:24:26 -0500

    renaming index.html to index.jsx
    express view engine

---
+++ views/index.jsx
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Blog</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+    <!-- Wrapper -->
+    <div>
+        Hello World
+    </div>
+</body>
+</html>
